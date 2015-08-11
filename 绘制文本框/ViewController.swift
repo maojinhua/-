d1e7e94287c88fa7fca2933a980694c3201beb62
Mao hua <maohua@maodemacbook-pro.local>
2015-08-11 17:13:22 +0800

    Initial Commit

---
+++ 绘制文本框/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  绘制文本框
+//
+//  Created by Mao hua on 15/8/11.
+//  Copyright © 2015年 Mao hua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
